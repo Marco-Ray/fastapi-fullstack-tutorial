2fd496780dfe42c31dc9c5835d04e484fd3203b0
Muyun <wangzehuan2017@sina.com>
2022-07-08 15:57:07 +0800

    PostgresQL

--- database.py
+++ database.py
@@ -1,20 +1,24 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 # MYSQL Series
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Wzh2017!@127.0.0.1:3306/todoapp"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-# # MYSQL Series
 # engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 # )
+
+# # MYSQL / Postgresql Series
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
